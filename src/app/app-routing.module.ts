fe971bcbbfb07ab5125a8eefdc9369c27fd4754f
R. Richards <rdrrichards@hotmail.com>
2020-01-08 19:48:05 -0600

    comms in viewer working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'viewer', pathMatch: 'full' },
+  { path: 'viewer', loadChildren: () => import('./viewer/viewer.module').then(m => m.ViewerModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommunicationService } from './communication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qiqo-ws-poc-viewer';
+  constructor(private commsService: CommunicationService) {}
+  ngOnInit() {
+    setTimeout(() => {
+      this.commsService.join();
+    }, 2000);
+  }
 }
